ac1bbaca27a272fa498d9b93833dcda5323ee84c
alunoinfo <alunoinfo@lab03m22>
2023-02-23 22:57:30 +0000

    corrigir erro

--- TRABALHO_GIT_2K/src/Main.java
+++ TRABALHO_GIT_2K/src/Main.java
@@ -5,14 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Olá Mundo");
-<<<<<<< HEAD
 		System.out.println("Olá Dev!");
-=======
 		Scanner t = new Scanner(System.in);
 		System.out.println("Digite o seu nome: ");
 		String a = t.nextLine();
 		System.out.println("Olá " + a);
->>>>>>> refs/heads/funcionalidade
 
 	}
 
